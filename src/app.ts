7a2b7630743e2347fe8b9eb7c3e1da8e7cc98257
Manu <manu020766@yahoo.es>
2020-06-03 10:24:52 +0200

    init project

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+
+const app = express()
+
+import indexRoutes from './routes/index'
+
+//settings
+app.set('port', process.env.PORT || 4000)
+
+//middlewares
+app.use(morgan('dev'))
+
+//routes
+app.use('/api',indexRoutes)
+
+// store public files
+app.use('/uploads', express.static(path.resolve('uploads')))
+
+export default app
